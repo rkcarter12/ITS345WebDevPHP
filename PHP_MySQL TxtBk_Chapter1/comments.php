dddadda5a6773c58bae263770027c9431e751808
Kent <rkcarter12@gmail.com>
2021-02-14 16:11:39 -0500

    New php file for displaying comments and basic output

---
+++ PHP_MySQL TxtBk_Chapter1/comments.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Comments</title>
+</head>
+    <body>
+    <?php
+        # Script 1.4 - comments.php
+        # Created February 14, 2021
+        # Created by Richard Carpenter
+        # This script does nothing much
+        
+        echo '<p>This is a line of text.<br>This is another line of text</p>';
+	?>
+    </body>
+</html>
