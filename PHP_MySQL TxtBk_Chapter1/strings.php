f1611d0d59768ce3674334f9b82830e0c7fe4fb2
Kent <rkcarter12@gmail.com>
2021-02-14 16:42:21 -0500

    New file strings.php

---
+++ PHP_MySQL TxtBk_Chapter1/strings.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Strings</title>
+</head>
+    <body>
+    <?php # Script 1.6 - strings.php
+        $first_name = 'Haruki';
+        $last_name = 'Murakami';
+        $book = 'Kafka on the Shore';
+	?>
+    </body>
+</html>
