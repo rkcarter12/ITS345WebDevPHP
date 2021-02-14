5b94350783d14612080f5b5d820c2f94a508ed29
Kent <rkcarter12@gmail.com>
2021-02-14 16:29:24 -0500

    New file added for showing predefined variables

---
+++ PHP_MySQL TxtBk_Chapter1/predefined.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Predefined Variables</title>
+</head>
+    <body>
+    <?php # Script 1.5 - predefined.php
+        $file = $_SERVER['SCRIPT_FILENAME']; // this is the full path and name of the script being run
+        $user = $_SERVER['HTTP_USER_AGENT']; // this is the browser and OS of the user
+        $server = $_SERVER['SERVER_SOFTWARE']; // this is the web application on the server that's running PHP
+        
+        echo "<p>You are running the file:<br> /><strong>$file</strong>.</p>\n";
+	?>
+    </body>
+</html>
