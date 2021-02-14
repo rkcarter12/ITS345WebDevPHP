05190e6128d04c308b8002043e16bb345a8d4769
Kent <rkcarter12@gmail.com>
2021-02-14 10:17:59 -0500

    Tested working with MySQL through MAMP web server

--- ITS345_MOD1_OPT1/ITS345_MOD1_OPT1_Carpenter_Richard.php
+++ ITS345_MOD1_OPT1/ITS345_MOD1_OPT1_Carpenter_Richard.php
@@ -1,2 +1,15 @@
-<?php
-    // Write a PHP program to swap two variables
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Insert title here</title>
+</head>
+    <body>
+    <p>
+    <?php
+        echo "Hello World!";
+        mysqli_connect('localhost','rkcarter','zetaNU766*tke','temp');
+	?>
+	</p>
+    </body>
+</html>
