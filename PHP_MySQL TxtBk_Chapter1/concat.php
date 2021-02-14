9b039f304473786bad62af7c858b717b0c6ed4b7
Kent <rkcarter12@gmail.com>
2021-02-14 16:52:13 -0500

    Addition of the concat.php file

---
+++ PHP_MySQL TxtBk_Chapter1/concat.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Strings</title>
+</head>
+    <body>
+    <?php # Script 1.6 - strings.php
+        $first_name = 'Haruki';
+        $last_name = 'Murakami';
+        $author = $first_name.' '.$last_name;
+        $book = 'Kafka on the Shore';
+        
+        echo "<p>The book <em>$book</em> was written by $author.</p>";
+	?>
+    </body>
+</html>
