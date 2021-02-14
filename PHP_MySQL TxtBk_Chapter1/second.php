080e462e88c90a227e99c7964bd3fdeec3183a6a
Kent <rkcarter12@gmail.com>
2021-02-14 16:02:54 -0500

    Second php file using PHP generated text

---
+++ PHP_MySQL TxtBk_Chapter1/second.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Chapter 1 Exercises</title>
+</head>
+    <body>
+    <p>
+    <?php
+        echo 'She said, "How are you?"<br>';
+        echo "I'm just ducky.<br>";
+        echo "She said, \"How are you?\"<br>";
+        echo 'I\'m just ducky.';
+        
+        echo "This was generated using PHP!";
+	?>
+	</p>
+    </body>
+</html>
