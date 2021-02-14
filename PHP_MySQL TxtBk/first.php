f4b6866e19f823ce45a72a065cebfae9016f98ae
Kent <rkcarter12@gmail.com>
2021-02-14 12:00:51 -0500

    Deleted and recreated the Project new instead of copying which worked

---
+++ PHP_MySQL TxtBk/first.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Insert title here</title>
+</head>
+    <body>
+    <p>
+    <?php
+        echo "Hello World!";
+        mysqli_connect('localhost','rkcarter','zetaNU766*tke','temp');
+	?>
+	</p>
+    </body>
+</html>
