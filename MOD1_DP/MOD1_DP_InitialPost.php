be6dfd872dfb6f8228e280e8efddfe36afb7d901
Kent <rkcarter12@gmail.com>
2021-02-14 19:28:00 -0500

    Updated the initial post to have single quotes to correct strlen()

---
+++ MOD1_DP/MOD1_DP_InitialPost.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+    <body>
+    <h1>First attempt at PHP here:</h1>
+    <?php
+    $name = 'Richard Carpenter';
+    
+    echo '<p>My name is '.$name.' and it is '.strlen($name).' characters long.</p>';
+    
+	?>
+    </body>
+</html>
