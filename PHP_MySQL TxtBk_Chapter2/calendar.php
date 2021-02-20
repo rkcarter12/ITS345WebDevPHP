6a150116441cd7eb3e7271fed3147cd5d56b2af6
Kent <rkcarter12@gmail.com>
2021-02-20 11:28:38 -0500

    New calendar.php file for arrays

---
+++ PHP_MySQL TxtBk_Chapter2/calendar.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Calendar</title>
+</head>
+    <body>
+    <form action="calendar.php" method="post">
+    <?php
+    $months = [1 => 'January','February','March','April','May','June','July','August','September','October','November','December'];
+	?>
+    </body>
+</html>
--- PHP_MySQL TxtBk_Chapter2/handle_form.php
+++ PHP_MySQL TxtBk_Chapter2/handle_form.php
@@ -6,7 +6,12 @@
 </head>
     <body>
     <?php # Script 2.5 - handle_form.php
-
+    if (!empty($_POST['name']) && !empty($_POST['comments']) && !empty($_POST['email'])) {
+        echo "<p>Thank you, <strong>{$_POST['name']}</strong>, for the following comments:</p><pre>{$_POST['comments']}</pre>
+        <p>We will reply to you at <em>{$_POST['email']}</em>.</p>\n";
+    } else {
+        echo '<p>Please go back and fill out the form again.</p>';
+    }
 	?>
     </body>
 </html>
