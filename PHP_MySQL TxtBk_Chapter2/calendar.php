944fbca74e19747d21b0c0ae39c059a7d8f2bf55
Kent <rkcarter12@gmail.com>
2021-02-20 12:12:23 -0500

    Adding the calendar.php example creating dropdowns using an array

--- PHP_MySQL TxtBk_Chapter2/calendar.php
+++ PHP_MySQL TxtBk_Chapter2/calendar.php
@@ -8,6 +8,30 @@
     <form action="calendar.php" method="post">
     <?php
     $months = [1 => 'January','February','March','April','May','June','July','August','September','October','November','December'];
+    $days = range(1,31);
+    $years = range(2017,2027);
+    
+    // Make the months pull-down menu
+    echo '<select name="month">';
+    foreach ($months as $key => $value) {
+        echo "<option value=\"$key\"> $value</option>\n";
+    }
+    echo '</select>';
+    
+    // Make the days pull-down menu
+    echo '<select name="day">';
+    foreach ($days as $value) {
+        echo "<option value=\"$key\"> $value</option>\n";
+    }
+    echo '</select>';
+    
+    // Make the years pull-down menu
+    echo '<select name ="years">';
+    foreach ($years as $value) {
+        echo "<option value=\"$key\"> $value</option>\n";
+    }
+    echo '</select>'; // closing tag for the 'select' HTML element
 	?>
+	</form>
     </body>
 </html>
