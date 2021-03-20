ce89623b063f90904419f0cee09f2d840ea611bc
Kent <rkcarter12@gmail.com>
2021-03-20 10:32:36 -0400

    File created prior to any code written (template)

---
+++ MOD6_CT_OPT1/ITS345_MOD6_OPT1_Carpenter_Richard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>MOD6_CT_OPT1</title>
+</head>
+    <body>
+    <?php # Program to return at least 50 rows from MySQL and display them on web page with pagination
+    
+    # Module 6 Critical Thinking Option #1
+    # Created 03/20/2021
+    # Created by Richard Carpenter
+    # This program will query a table in the database and display those results in basic data grid with paging
+    
+    
+	?>
+    </body>
+</html>
