32b631b899817a6adde1d55215bb401aa2bc3ffd
Kent <rkcarter12@gmail.com>
2021-04-01 23:04:13 -0400

    Updates to remove unneeded variable

--- MOD8_DP/MOD8_DP_SalesOrder.php
+++ MOD8_DP/MOD8_DP_SalesOrder.php
@@ -10,34 +10,40 @@
         private $createDate;
         private $orderType = array('Quote','Cash','Credit');
         private $amount;
-        
+       
+       //sets the current date with formatting 
        public function getDate() {
            $this->date = new DateTime('today'); 
            return $this->date->format('D,&\nb\sp;M&\nb\sp;jS Y');
         }
-        
+       
+        //sets the type by updating the private variable via the setter for Order Type
         public function setType($orderType) {
             $this->type = ($orderType);
         }
+        //retrieves the Order Type
         public function getType() {
             return $this->type;
         }
         
+        //sets the amount by updating the private variable via the setter for Amount
         public function setAmount($amount) {
             $this->total = $amount;
         }
+        //retrieves the Amount
         public function getAmount() {
             return $this->total;
         }
         
     }
     
+    //Instantiate a new object from the class, set the amount and type
     $salesOrder1 = new MOD8_DP_SalesOrder();
     $salesOrder1->setAmount(1200.74);
     $salesOrder1->setType('Cash');
-    $saleDate = $salesOrder1->getDate();
-            
-    echo "<p>On ".$saleDate." a ".$salesOrder1->getType()." sales order was placed in the amount of $".$salesOrder1->getAmount().".<p>";
+    
+    //gets the date, sales order type and amount base on the object created and displays them as part of the output
+    echo "<p>On ".$salesOrder1->getDate()." a ".$salesOrder1->getType()." sales order was placed in the amount of $".$salesOrder1->getAmount().".<p>";
     
 	?>
     </body>
